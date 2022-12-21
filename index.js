33fb35bd600b173d4e7ffca1c6673c6e428159af
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2022-12-21 11:05:12 +0000

    WIP

--- index.js
+++ index.js
@@ -3,37 +3,26 @@
 const soap = require('strong-soap').soap;
 // wsdl of the web service this client is going to invoke.
 const config = require('./config');
-const { getCase } = require('./models/ims-model');
+const { createCase, getCase } = require('./models/ims-model');
 const wsdlUrl = config.ims.wsdl;
-
-// const requestArgs = {
-//   ClassificationEventCode:'4000041'
-// };
 
 const options = {};
 
-// const requestArgs = {
-//   FWTCaseCreate : {
-//     ClassificationEventCode: '4000041'
-//   }
-// };
-// const auth = `Basic:${Buffer.from(`${config.ims.apiUser}:${config.ims.apiPassword}`).toString('base64')}`;
-// soap.createClient(wsdlUrl, { wsdl_headers: { Authorization: auth } }, function(err, client) {
-//   client.setSecurity(new soap.BasicAuthSecurity(, ));
-//   var method = client['createCase'];
-//   method(requestArgs, function(err, result, envelope, soapHeader) {
-//     console.log('Response Envelope: \n' + envelope);
-//     console.log('Result: \n' + JSON.stringify(result));
-//   });
-// });
+const caseType = {
+  FWTCaseCreate : {
+    ClassificationEventCode: config.ims.PublicAllegationsEventCode
+  }
+};
 
-const requestArgs = {
+const caseOptions = {
   FWTCaseFullDetailsRequest : {
-    CaseReference: '101000002951',
+    CaseReference: '101000002989',
     Option: 'all'
   }
 };
 
-getCase(requestArgs);
+createCase(caseType);
+getCase(caseOptions);
+
 
 
--- models/ims-model.js
+++ models/ims-model.js
@@ -4,26 +4,36 @@ const config = require('../config');
 const wsdlUrl = config.ims.wsdl;
 
 module.exports = {
-  createCase: () => {},
+  /** **********************************************************
+   * @param {Object} caseType type of case to create
+   * @param {Object} caseType.FWTCaseCreate
+   * @param {String} caseType.FWTCaseCreate.ClassificationEventCode
+   ************************************************************/
 
-  // getCase: (requestArgs) => {
-  //   const auth = `Basic:${Buffer.from(`${config.ims.apiUser}:${config.ims.apiPassword}`).toString('base64')}`;
-  //   soap.createClient(wsdlUrl, { wsdl_headers: { Authorization: auth } }, function(err, client) {
-  //     client.setSecurity(new soap.BasicAuthSecurity(config.ims.apiUser, config.ims.apiPassword));
-  //     var method = client[config.ims.getCaseApi];
-  //     method(requestArgs, function(err, result, envelope, soapHeader) {
-  //       console.log('Response Envelope: \n' + envelope);
-  //       console.log('Result: \n' + JSON.stringify(result, null, 2));
-  //     });
-  //   })
-  // }
+  
 
-  getCase: (requestArgs) => {
+  createCase: (caseType) => {
+    const auth = `Basic:${Buffer.from(`${config.ims.apiUser}:${config.ims.apiPassword}`).toString('base64')}`;
+    soap.createClient(wsdlUrl, { wsdl_headers: { Authorization: auth } }, async function(err, client) {
+      client.setSecurity(new soap.BasicAuthSecurity(config.ims.apiUser, config.ims.apiPassword));
+      var method = client[config.ims.createCaseApi];
+      const {result, envelope, soapHeader} = await client.createCase(caseType);
+      console.log('Response Envelope: \n' + envelope);
+      console.log('Result: \n' + JSON.stringify(result, null, 2));
+    });
+  },
+  /** **********************************************************
+   * @param {Object} caseOptions case details
+   * @param {Object} caseOptions.FWTCaseFullDetailsRequest 
+   * @param {String} caseOptions.FWTCaseFullDetailsRequest.CaseReference
+   * @param {String} caseOptions.FWTCaseFullDetailsRequest.Option
+   ************************************************************/
+  getCase: (caseOptions) => {
     const auth = `Basic:${Buffer.from(`${config.ims.apiUser}:${config.ims.apiPassword}`).toString('base64')}`;
     soap.createClient(wsdlUrl, { wsdl_headers: { Authorization: auth } }, async function (err, client) {
       client.setSecurity(new soap.BasicAuthSecurity(config.ims.apiUser, config.ims.apiPassword));
       var method = client[config.ims.getCaseApi];
-      const {result, envelope, soapHeader} = await client.retrieveCaseDetails(requestArgs);
+      const {result, envelope, soapHeader} = await client.retrieveCaseDetails(caseOptions);
       console.log('Response Envelope: \n' + envelope);
       console.log('Result: \n' + JSON.stringify(result, null, 2));
     });
