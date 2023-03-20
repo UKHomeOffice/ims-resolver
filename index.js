2fc1b16a1608ecc7825cb9e2ee5f9a4af834c386
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2023-03-20 15:09:50 +0000

    IMS API functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+"use strict";
+
+const soap = require('strong-soap').soap;
+const config = require('./config');
+const { createPublicAllegationsCase } = require('./models/ims-model');
+
+const caseType = {
+  FWTCaseCreate : {
+    ClassificationEventCode: config.ims.PublicAllegationsEventCode
+  }
+};
+
+createPublicAllegationsCase(caseType);
