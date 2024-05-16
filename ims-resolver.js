a27665d76d93452663cf0c6ee42d1a8b50e9e0ea
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-05-16 12:57:49 +0100

    Add reject param to the handleMessage promise

--- ims-resolver.js
+++ ims-resolver.js
@@ -32,7 +32,7 @@ const imsResolver = {
   },
 
   handleMessage: async message => {
-    return new Promise(async resolve => {
+    return new Promise(async (resolve, reject) => {
       const messageBody = JSON.parse(message.Body);
       // console.log(messageBody);
 
